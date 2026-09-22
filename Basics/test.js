98c2de117de68b676ea6b0210038409f8237c198
Ayush Singh <ayushsingh11225@gmail.com>
2026-09-22 21:53:17 +0530

    Update test.js

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,19 @@
-console.log("Ayush"); 
+Ayush
+JavaScript Test Started
+
+--- VARIABLES ---
+Name: Ayush
+Age: 29
+City: Delhi
+
+--- DATA TYPES ---
+String: Hello JavaScript
+Number: 100
+Decimal: 99.99
+Boolean: true
+
+...
+
+================================
+JAVASCRIPT TEST COMPLETED
+================================
