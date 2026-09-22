c47f13fec0c9febdd9828d87f5b6362ff2f5400e
Ayush Singh <ayushsingh11225@gmail.com>
2026-09-22 21:45:21 +0530

    Update 01_veriables.js

--- Basics/01_veriables.js
+++ Basics/01_veriables.js
@@ -1,6 +1,6 @@
 const accountId = 123456;
 let accountEmail = "ayush@google.com"
-var accountPassword = "123456";
+var accountPassword = "123456"; 
 accountCity = "New Delhi";
 
 console.table({ accountId, accountEmail, accountPassword, accountCity });
