a7b3353cc967056ab5400fdf584456661d9adb78
Ayush Singh <ayushsingh11225@gmail.com>
2026-09-23 22:37:29 +0530

    Implement score conversion examples
    Added type conversion examples using Number() function.

--- Basics/03_conversioOperation.js
+++ Basics/03_conversioOperation.js
@@ -1,1 +1,16 @@
-c
+let score = "35"; 
+let score1 = "35aa";
+
+console.log(typeof score); 
+console.log(typeof score);
+
+let convertedScore = Number(score);
+
+let convertedScore1 = Number(score1);
+ 
+console.log(typeof convertedScore); 
+console.log(convertedScore);  
+
+console.log(typeof convertedScore1);  
+console.log(convertedScore1);
+console.log(convertedScore1);
